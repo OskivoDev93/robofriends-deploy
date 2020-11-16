81e21e80f989ab809f71f8e2ac5d5424d7f2ee31
OskivoDev93 <49086138+OskivoDev93@users.noreply.github.com>
2020-11-16 16:47:11 +0800

    my react main files

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jojo',
+        username: 'jojo123',
+        email: 'jojo123@gmail.com',
+    },
+    {
+        id:21,
+        name: 'Leanthal',
+        username: 'Lea72',
+        email: 'Lea72@gmail.com',
+    },
+    {
+        id: 3,
+        name: 'Darumba',
+        username: 'Daru28',
+        email: 'Daru28@gmail.com',
+    },
+    {
+        id: 4,
+        name: 'Kusharn',
+        username: 'Kush420',
+        email: 'Kush4203@gmail.com',
+    },
+    {
+        id: 5,
+        name: 'Gilette',
+        username: 'Gilt83',
+        email: 'Gilt83@gmail.com',
+    },
+    {
+        id: 6,
+        name: 'Torbjorn',
+        username: 'Bjorn93',
+        email: 'Bjorn93@gmail.com',
+    },
+    {
+        id: 7,
+        name: 'Durandal',
+        username: 'Duran22',
+        email: 'Duran2@gmail.com',
+    },
+    {
+        id: 8,
+        name: 'Lagertha',
+        username: 'Ertha3',
+        email: 'Ertha3@gmail.com',
+    },
+    {
+        id: 9,
+        name: 'Lifendel',
+        username: 'Leaf73',
+        email: 'Leaf73@gmail.com',
+    },
+    {
+        id: 10,
+        name: 'Jaegar',
+        username: 'Jaegar93',
+        email: 'Jaegar93@gmail.com',
+    },
+
+]
